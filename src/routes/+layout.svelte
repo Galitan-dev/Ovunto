5e52806ba0572e848311f8516c14784a929033da
Galitan-dev <galitan.dev@gmail.com>
2023-08-14 15:44:39 +0200

    feat: header

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,13 @@
 <script lang="ts">
-	import '../app.less';
+    import '../app.less';
+    import Header from './Header.svelte';
 </script>
 
 <svelte:head>
-	<title>Ovunto: Your Privacy, Your Egg!</title>
+    <title>Ovunto: Your Privacy, Your Egg!</title>
 </svelte:head>
 
-<slot />
+<Header />
+<main>
+    <slot />
+</main>
---
+++ src/routes/Header.svelte
@@ -0,0 +1,57 @@
+<script lang="ts">
+    import logo from '$lib/images/logo.svg';
+    import githubLogo from '$lib/images/github_logo.png';
+    import { PUBLIC_REPOSITORY_URL } from '$env/static/public';
+</script>
+
+<header>
+    <a
+        class="logo"
+        href="/"
+    >
+        <img
+            src="{logo}"
+            alt="Ovunto Logo"
+        />
+        <h1>Ovunto</h1>
+    </a>
+    <a
+        href="{PUBLIC_REPOSITORY_URL}"
+        target="_blank"
+    >
+        <img
+            src="{githubLogo}"
+            alt="GitHub Logo"
+        />
+    </a>
+</header>
+
+<style lang="less">
+    @import '$lib/theme.less';
+
+    header {
+        display: flex;
+        align-items: center;
+        height: 60px;
+        padding: 0 20px;
+        background-color: darken(@background-color, 10%);
+
+        :last-child {
+            margin-left: auto;
+        }
+    }
+
+    .logo {
+        display: flex;
+        align-items: center;
+        justify-content: start;
+        width: min-content;
+        gap: 10px;
+        color: inherit;
+        text-decoration: none;
+    }
+
+    img {
+        height: 40px;
+    }
+</style>
