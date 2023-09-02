cfc687f65d085f71f4888e342721e0089b983d8b
Galitan-dev <galitan.dev@gmail.com>
2023-09-02 16:19:39 +0200

    fix: import logo from assets

--- src/lib/images/logo/index.ts
+++ src/lib/images/logo/index.ts
@@ -1,7 +1,7 @@
-import './day.svg';
-import './night.svg';
+import day from './day.svg';
+import night from './night.svg';
 
 import { theme } from '$lib/stores/preferences';
 import { derived } from 'svelte/store';
 
-export default derived(theme, (theme) => `/src/lib/images/logo/${theme}.svg`);
+export default derived(theme, (theme) => ({ day, night })[theme]);
